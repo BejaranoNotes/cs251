06102cd0400e71bbd17eb0042a9abc163fd55833
Siddhanth Kumar <53668449+sidgithub780@users.noreply.github.com>
2024-09-12 12:27:46 -0400

    fixed order of notes

--- src/SemesterPage.tsx
+++ src/SemesterPage.tsx
@@ -19,12 +19,14 @@ const SemesterPage: React.FC = () => {
   Object.keys(notes).forEach((path) => {
     const match = path.match(new RegExp(`/notes/${semester}/([^/]+)/([^/]+)/`))
     if (match) {
+      console.log("match", match)
       const week = match[1]
       const date = match[2]
       if (!weeksMap.has(week)) {
         weeksMap.set(week, new Set<string>())
       }
       weeksMap.get(week)?.add(date)
+      console.log(weeksMap)
     }
   })
 
@@ -37,21 +39,28 @@ const SemesterPage: React.FC = () => {
         <div key={week}>
           <h2 className="mt-2 text-lg font-semibold">{formatWeek(week)}</h2>
           <ul>
-            {Array.from(dates).map((date) => {
-              const fullDateStr = getFullDate(semester!, date)
-              const parsedDate = parse(fullDateStr, "yyyy-M-d", new Date())
-              const dayOfWeek = format(parsedDate, "EEEE")
-              return (
-                <li key={date}>
-                  <Link
-                    to={`/${semester}/${date}`}
-                    className="text-blue-500 underline"
-                  >
-                    {formatDate(date)} ({dayOfWeek})
-                  </Link>
-                </li>
-              )
-            })}
+            {Array.from(dates)
+              .sort((a, b) => {
+                const dateA = new Date(getFullDate(semester!, a))
+                const dateB = new Date(getFullDate(semester!, b))
+                return dateA.getTime() - dateB.getTime()
+              })
+              .map((date) => {
+                console.log(date)
+                const fullDateStr = getFullDate(semester!, date)
+                const parsedDate = parse(fullDateStr, "yyyy-M-d", new Date())
+                const dayOfWeek = format(parsedDate, "EEEE")
+                return (
+                  <li key={date}>
+                    <Link
+                      to={`/${semester}/${date}`}
+                      className="text-blue-500 underline"
+                    >
+                      {formatDate(date)} ({dayOfWeek})
+                    </Link>
+                  </li>
+                )
+              })}
           </ul>
         </div>
       ))}
